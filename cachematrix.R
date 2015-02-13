11c802f06b5333ce474463b5fd0755337be75f61
Jessica Helsten <jhelsten@gmail.com>
2015-02-13 18:12:55 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below is code to create a matrix to test the function
+##
 ## create maxtrix 
 ## x <- matrix(c(2, 4, 3, 1, 5, 7, 8, 9, 10), nrow=3, ncol=3)
 ## [,1] [,2] [,3]
 ##    2    1    8
 ##    4    5    9
 ##    3    7   10
+## The CacheSolve(makeCacheMatrix(x)) should return the below numbers
 ## solve(x)
 ##     [,1]        [,2]        [,3]
 ## -0.2  0.70769231 -0.47692308
 ## -0.2 -0.06153846  0.21538462
 ##  0.2 -0.16923077  0.09230769
-## Write a short comment describing this function
-
+##
+## This is a function to cache the value of the matrix inverse
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
@@ -27,7 +28,8 @@ makeCacheMatrix <- function(x = matrix()) {
              getinverse = getinverse)
 }
 
-## Write a short comment describing this function
+## This function either brings back the value of the cached variable
+## or does the solve function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
